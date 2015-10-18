502288effd1c89a57ea16c0e0876b88369e4420d
Sven Steudte <sven.steudte@gmail.com>
2015-10-18 11:02:39 +0200

    Attempt to fix ADC misreading

--- src/adc.c
+++ src/adc.c
@@ -17,7 +17,7 @@ void ADC_Init(void) {
 	while(!Chip_ADC_IsCalibrationDone(LPC_ADC));
 
 	// Configure clock
-	// Chip_ADC_SetClockRate(LPC_ADC, 1000); // Clock 1kHz
+	Chip_ADC_SetClockRate(LPC_ADC, 1000); // Clock 1kHz
 }
 
 void ADC_DeInit(void) {
@@ -50,21 +50,19 @@ uint32_t getSolarMV(void)
 uint32_t getADC(uint8_t ad)
 {
 	// Start ADC conversion
-	Chip_ADC_SetupSequencer(LPC_ADC, ADC_SEQA_IDX, ADC_SEQ_CTRL_CHANSEL(ad) | ADC_SEQ_CTRL_MODE_EOS);
+	Chip_ADC_SetupSequencer(LPC_ADC, ADC_SEQA_IDX, ADC_SEQ_CTRL_CHANSEL(ad) | ADC_SEQ_CTRL_MODE_EOS | ADC_SEQ_CTRL_HWTRIG_POLPOS);
 	Chip_ADC_EnableSequencer(LPC_ADC, ADC_SEQA_IDX);
-	Chip_ADC_StartBurstSequencer(LPC_ADC, ADC_SEQA_IDX);
 
 	uint32_t i = 0;
-	uint32_t seq_gdat = 0;
-	while((seq_gdat & ADC_SEQ_GDAT_DATAVALID) == 0)
+	uint32_t gdat = 0;
+	while((gdat & (ADC_DR_OVERRUN | ADC_SEQ_GDAT_DATAVALID)) == 0)
 	{
 		// Retrieve sampled data
-		seq_gdat = Chip_ADC_GetSequencerDataReg(LPC_ADC, ADC_SEQ_CTRL_CHANSEL(ad));
-		if(i > 1000) {
-			return 0;
-		}
-		i++;
+		gdat = Chip_ADC_GetDataReg(LPC_ADC, ad);
+
+		if(i++ > 1000)
+			return 0; // It took too long to sample the data
 	}
 
-	return (seq_gdat & ADC_SEQ_GDAT_RESULT_MASK) >> ADC_SEQ_GDAT_RESULT_BITPOS; // Cut out 12bit value
+	return (gdat & ADC_SEQ_GDAT_RESULT_MASK) >> ADC_SEQ_GDAT_RESULT_BITPOS; // Cut out 12bit value
 }
